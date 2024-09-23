0acf9e73a99ee7630ef6a1eda083e87821221f9f
corn <dalcon10280@gmail.com>
2024-09-23 20:02:22 +0900

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "mango_shake"
---
+++ src/test/kotlin/why_mango/mango_shake/MangoShakeApplicationTests.kt
@@ -0,0 +1,13 @@
+package why_mango.mango_shake
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MangoShakeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
