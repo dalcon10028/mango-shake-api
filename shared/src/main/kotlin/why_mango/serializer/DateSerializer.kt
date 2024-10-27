b70f1f81ae460bd74ae2dc3a261a971d78889cdf
corn <dalcon10280@gmail.com>
2024-10-27 18:38:29 +0900

    fix: serialize with iso formater

--- shared/src/main/kotlin/why_mango/serializer/DateSerializer.kt
+++ shared/src/main/kotlin/why_mango/serializer/DateSerializer.kt
@@ -4,15 +4,18 @@ import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.*
 import kotlinx.serialization.encoding.*
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 object DateSerializer: KSerializer<LocalDate> {
+    private val formatter = DateTimeFormatter.ISO_DATE_TIME
+
     override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("LocalDate", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: LocalDate) {
-        encoder.encodeString(value.toString())
+        encoder.encodeString(value.format(formatter))
     }
 
     override fun deserialize(decoder: Decoder): LocalDate {
-        return LocalDate.parse(decoder.decodeString())
+        return LocalDate.parse(decoder.decodeString(), formatter)
     }
 }
--- shared/src/main/kotlin/why_mango/serializer/DateTimeSerializer.kt
+++ shared/src/main/kotlin/why_mango/serializer/DateTimeSerializer.kt
@@ -4,15 +4,18 @@ import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.*
 import kotlinx.serialization.encoding.*
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 object DateTimeSerializer: KSerializer<LocalDateTime> {
+    private val formatter = DateTimeFormatter.ISO_DATE_TIME
+
     override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("LocalDateTime", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: LocalDateTime) {
-        encoder.encodeString(value.toString())
+        encoder.encodeString(value.format(formatter))
     }
 
     override fun deserialize(decoder: Decoder): LocalDateTime {
-        return LocalDateTime.parse(decoder.decodeString())
+        return LocalDateTime.parse(decoder.decodeString(), formatter)
     }
 }
