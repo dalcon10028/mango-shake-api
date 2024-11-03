4ec45f7456917f205b265abb1191d1005039e358
corn <dalcon10280@gmail.com>
2024-11-03 19:38:38 +0900

    chore: add authenticationEntryPoint

--- app/src/main/kotlin/why_mango/config/WebSecurityConfig.kt
+++ app/src/main/kotlin/why_mango/config/WebSecurityConfig.kt
@@ -3,6 +3,7 @@ package why_mango.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.authorization.method.AuthorizationAdvisorProxyFactory.withDefaults
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
@@ -12,9 +13,9 @@ import org.springframework.security.config.web.server.ServerHttpSecurity
 
 // https://stackoverflow.com/questions/66018084/how-to-enable-spring-security-kotlin-dsl
 import org.springframework.security.config.web.server.invoke
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers.pathMatchers
 import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.reactive.CorsConfigurationSource
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 import why_mango.auth.AuthenticationSuccessHandler
 import why_mango.auth.JwtAuthenticationFilter
@@ -27,7 +28,7 @@ class WebSecurityConfig(
     private val env: EnvironmentComponent,
     private val jwtAuthenticationFilter: JwtAuthenticationFilter,
     private val serverAuthenticationSuccessHandler: AuthenticationSuccessHandler,
-    @Value("\${app.web-base-url}") private val webBaseUrl: String,
+    @Value("\${app.domain}") private val appDomain: String,
 ) {
 
     @Bean
@@ -44,12 +45,14 @@ class WebSecurityConfig(
             formLogin { withDefaults() }
             httpBasic { disable() }
             csrf { disable() }
-//            cors { corsConfigurationSource() }
-            cors { disable() }
+            cors { corsConfigurationSource() }
             // https://velog.io/@yso8296/Spring-Security를-이용한-통합-OAuth2-소셜-로그인-기능-구현
             // https://velog.io/@rkdalstj4505/스프링-시큐리티OAuth2카카오로-로그인로그아웃-구현
             oauth2Login {
                 authenticationSuccessHandler = serverAuthenticationSuccessHandler
+            }
+            exceptionHandling {
+                authenticationEntryPoint = HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)
             }
             addFilterBefore(jwtAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
             sessionManagement { SessionCreationPolicy.STATELESS }
@@ -57,12 +60,9 @@ class WebSecurityConfig(
     }
 
     @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource {
+    fun corsConfigurationSource(): UrlBasedCorsConfigurationSource {
         val configuration = CorsConfiguration().also {
-            it.allowedOrigins = listOf(
-                webBaseUrl,
-                "http://localhost:3000" // TODO: 환경 분리시 cors 설정 변경
-            )
+            it.addAllowedOriginPattern(appDomain)
             it.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
             it.allowedHeaders = listOf("*")
             it.allowCredentials = true
