733611de3a8259908e05e9ccb27186c31eab69d6
corn <dalcon10280@gmail.com>
2025-02-01 13:57:10 +0900

    feat: add windowed extension function for Flow

---
+++ shared/src/main/kotlin/why_mango/utils/FlowUtils.kt
@@ -0,0 +1,22 @@
+package why_mango.utils
+
+import kotlinx.coroutines.flow.*
+
+/**
+ * Emit a list of values that are collected in a sliding window.
+ * The window is moved by one element at a time.
+ */
+suspend fun <T> Flow<T>.windowed(size: Int): Flow<List<T>> = flow {
+    require(size > 0) { "Window size must be greater than 0" }
+    val window = ArrayDeque<T>(size)
+    collect { value ->
+        if (window.size == size) {
+            window.removeFirst()
+        }
+        window.add(value)
+        if (window.size == size) {
+            // window의 복사본을 방출 (immutable한 List)
+            emit(window.toList())
+        }
+    }
+}
