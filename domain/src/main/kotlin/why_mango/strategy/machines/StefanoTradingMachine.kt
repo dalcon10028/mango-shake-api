d04fa5489b3eab727b2609df91839d9e13278dcb
corn <dalcon10280@gmail.com>
2025-02-12 17:44:20 +0900

    내 돈 갈린다 ㅠㅠ 손익비 조정

--- domain/src/main/kotlin/why_mango/strategy/machines/StefanoTradingMachine.kt
+++ domain/src/main/kotlin/why_mango/strategy/machines/StefanoTradingMachine.kt
@@ -123,8 +123,8 @@ class StefanoTradingMachine(
                     SYMBOL,
                     size = (BALANCE_USD.toBigDecimal() / stefano.price).setScale(0),
                     price = stefano.price,
-                    presetStopSurplusPrice = stefano.price.takeProfitLong(0.1, 10).setScale(3, RoundingMode.FLOOR),
-                    presetStopLossPrice = stefano.price.stopLossLong(0.07, 10).setScale(3, RoundingMode.FLOOR)
+                    presetStopSurplusPrice = stefano.price.takeProfitLong(0.15, 10).setScale(3, RoundingMode.FLOOR),
+                    presetStopLossPrice = stefano.price.stopLossLong(0.1, 10).setScale(3, RoundingMode.FLOOR)
                 )
                 publisher.publishEvent(
                     SlackEvent(
@@ -147,8 +147,8 @@ class StefanoTradingMachine(
                     SYMBOL,
                     size = (BALANCE_USD.toBigDecimal() / stefano.price).setScale(0),
                     price = stefano.price,
-                    presetStopSurplusPrice = stefano.price.takeProfitShort(0.1, 10).setScale(3, RoundingMode.FLOOR),
-                    presetStopLossPrice = stefano.price.stopLossShort(0.07, 10).setScale(3, RoundingMode.FLOOR)
+                    presetStopSurplusPrice = stefano.price.takeProfitShort(0.15, 10).setScale(3, RoundingMode.FLOOR),
+                    presetStopLossPrice = stefano.price.stopLossShort(0.1, 10).setScale(3, RoundingMode.FLOOR)
                 )
                 publisher.publishEvent(
                     SlackEvent(
