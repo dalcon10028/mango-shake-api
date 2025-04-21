2d2613ea1113b8b230ab4a267c04586f2a7be2bd
corn <dalcon10280@gmail.com>
2025-04-21 13:10:59 +0900

    fix: fix get order size logic

--- domain/src/main/kotlin/why_mango/strategy/bollinger_bands_trend/BollingerBandTrendTradingMachine.kt
+++ domain/src/main/kotlin/why_mango/strategy/bollinger_bands_trend/BollingerBandTrendTradingMachine.kt
@@ -330,13 +330,20 @@ class BollingerBandTrendTradingMachine(
         // TODO: Cache
         suspend fun orderSize(symbol: String, price: BigDecimal): BigDecimal {
             val contractConfig = bitgetFutureService.getContractConfig(symbol)
-            val sizeMultiplier = contractConfig.sizeMultiplier
-            val rawSize = properties.entryAmount * properties.leverage / price
+            val sizeMultiplier = contractConfig.sizeMultiplier            // 0.01
 
-            // rawSize가 sizeMultiplier의 몇 배인지 계산 후, 그 배수에 맞춰 조정
-            val multiplierCount = rawSize.divide(sizeMultiplier, 0, RoundingMode.DOWN)
-            return multiplierCount.multiply(sizeMultiplier)
-                // sizeMultiplier의 소수점 자릿수를 유지하도록 setScale
+            // 자본 = entryAmount * leverage
+            val capital = properties.entryAmount.multiply(properties.leverage)
+
+            // 원시 계산: 자본 / 가격 (충분한 소수점 자리로 계산)
+            val rawSize = capital.divide(price, price.scale() + 8, RoundingMode.DOWN)
+
+            // sizeMultiplier 의 정수 배수만큼 뽑아내기
+            val multiplierCount = rawSize.divideToIntegralValue(sizeMultiplier)
+
+            // 최종 수량: multiplierCount * sizeMultiplier, 소수점 자리(sizeMultiplier.scale()) 유지
+            return multiplierCount
+                .multiply(sizeMultiplier)
                 .setScale(sizeMultiplier.stripTrailingZeros().scale(), RoundingMode.DOWN)
         }
 
--- domain/src/test/kotlin/why_mango/strategy/machines/BollingerBandTradingMachineTest.kt
+++ domain/src/test/kotlin/why_mango/strategy/machines/BollingerBandTradingMachineTest.kt
@@ -10,7 +10,11 @@ class BollingerBandTradingMachineTest(
     private val bollingerBandTradingMachine: BollingerBandTrendTradingMachine
 ) : FunSpec({
 
-    test("orderSize") {
+    test("orderSize ETH") {
         bollingerBandTradingMachine.orderSize("ETHUSDT", 2202.96.toBigDecimal()) shouldBe 0.22.toBigDecimal()
     }
+
+//    test("orderSize PEPE") {
+//        bollingerBandTradingMachine.orderSize("PEPEUSDT", 0.000012345678901234.toBigDecimal()) shouldBe 4050000036.4501.toBigDecimal()
+//    }
 })
